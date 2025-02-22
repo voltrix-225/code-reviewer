d59043b930c227b29819c8078a95ed45f9a8c650
voltrix-225 <voltrix.coding@gmail.com>
2025-02-23 02:46:44 +0530

    Created scores.py module

--- codeReviewAnalyser/src/analyzers/scores.py
+++ codeReviewAnalyser/src/analyzers/scores.py
@@ -1,1 +1,10 @@
 #checks pylint score
+
+import subprocess
+import re
+
+def check_score(fp):
+    score = subprocess.run(["pylint", fp], capture_output=True, text = True)
+    match = re.search(r"Your code has been rated at ([\d\.]+)/10", score.stdout)
+    return float(match.group(1)) 
+
